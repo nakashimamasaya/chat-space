a36dde2e12d6b8957f623399dfa0f2776f011c50
Masaya Nakashima <kawa18sima@gmail.com>
2017-04-30 14:28:09 +0900

    Edit routing
    Add routing description of groups relation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'messages#index'
+  root 'groups#index'
+
+  resources :groups ,only: [:edit,:new] do
+    resources :messages,only: [:index]
+  end
 
 end
