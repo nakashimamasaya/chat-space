7fc959210105c8564b50fd7314cc64ee7c1db9d8
Masaya Nakashima <kawa18sima@gmail.com>
2017-05-09 00:34:29 +0900

    Edit messages controller
    Index action
    · Group group information on the page
    · Group to which the user belongs
    When sending a message
    · Group message
    Add instance variable
    
    Create action
    Save processing of transmission data
    Redirect processing to index
    Add

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,22 @@
 class MessagesController < ApplicationController
   def index
-    @messages = ""
+    @group = Group.find(params[:group_id])
     @groups = current_user.groups
+    @message = Message.new
+    @messages = @group.messages
+  end
+
+  def create
+    message = Message.new(message_params)
+    if message.save
+    else
+    end
+    redirect_to group_messages_path(params[:group_id])
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body).merge(group_id: params[:group_id],user_id: current_user.id)
   end
 end
