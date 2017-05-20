a0e7b0f47727529ee8666fcc83a7d7b488d089cb
Masaya Nakashima <kawa18sima@gmail.com>
2017-05-20 21:40:42 +0900

    Added processing for json in messages_controller.rb
    Add processing when sending json at create action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,7 +9,11 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
     if @message.save
-      redirect_to group_messages_path(@message.group_id), notice:  "メッセージ送信成功"
+      flash.now[:notice] = "メッセージ送信成功"
+      respond_to do |format|
+        format.html { redirect_to group_messages_path(@message.group_id), notice:  "メッセージ送信成功"}
+        format.json
+      end
     else
       flash.now[:alert] = "メッセージを入力してください"
       render :index
