e571dc12186ea36f7606ef1463280d6c9e657394
Masaya Nakashima <kawa18sima@gmail.com>
2017-05-20 21:42:17 +0900

    Create messages.js
    Describe ajax processing when message is sent, processing on flash
    message

---
+++ app/assets/javascripts/messages.js
@@ -0,0 +1,58 @@
+$(function() {
+  function buildHTML(message) {
+    var html = `<div class="messages__name" id="li">
+                  ${ message.name }
+                </div>
+                <div class="messages__date" id="li">
+                  ${ message.date }
+                </div>
+                <div class="messages__text" id="li">
+                  ${ message.body }
+                </div>`;
+    return html;
+  }
+
+  function buildFlashNotice(message){
+    var flash = `<p class="flash-message-notice">
+                  ${message.flash}
+                 </p>`;
+    return flash;
+  }
+
+  function buildFlashAlert(alert){
+    var flash = `<p class="flash-message-alert">
+                  ${alert}
+                 </p>`;
+    return flash;
+  }
+
+  $('.js-form').on('submit', function(e) {
+    e.preventDefault();
+    flashMessage.remove() if (var flashMessage = $('.flash-message-notice') || flashMessage = $('.flash-message-alert'));
+    var textField = $('.post-space__text');
+    var message = textField.val();
+    $.ajax({
+      type: 'POST',
+      url: './messages',
+      data: {
+        message:{
+          body: message
+        }
+      },
+      dataType: 'json'
+    })
+    .done(function(message) {
+      var html = buildHTML(message);
+      $('.messages__list').append(html);
+      var flash = buildFlashNotice(message);
+      $('body').prepend(flash);
+      $('.js-form')[0].reset();
+    })
+    .fail(function() {
+      var alert = "メッセージを入力してください。";
+      var flash = buildFlashAlert(alert);
+      $('body').prepend(flash);
+    });
+    return false;
+  });
+});
