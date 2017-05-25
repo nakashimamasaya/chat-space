c34dba6820b2aaa4252ea943270a737cc41a99d1
Masaya Nakashima <kawa18sima@gmail.com>
2017-05-26 03:28:39 +0900

    Create groups.js
    Add incremental search process

---
+++ app/assets/javascripts/groups.js
@@ -0,0 +1,57 @@
+$(function() {
+  function buildHTML(user) {
+    var html = `<li class="chat-group-user">
+                  <div class="chat-group-user__name">${ user.name }</div>
+                  <div class="chat-group-user__btn chat-group-user__btn--add" data-name="${ user.name }" data-id="${ user.id }">追加</div>
+                </li>`;
+    return html;
+  }
+
+  function removeHTML(name, id) {
+    var html = `<li class="chat-group-user">
+                  <div class="chat-group-user__name">${ name }</div>
+                  <div class="chat-group-user__btn chat-group-user__btn--remove">削除</div>
+                  <input type='hidden' value="${ id }" name="group[user_ids][]">
+                </li>`;
+    return html;
+  }
+
+  $(document).on('click', '.chat-group-user__btn--add', function(e){
+    var name = $(this).data('name');
+    var id = $(this).data('id');
+    var removeHtml = removeHTML(name,id);
+    $('#chat-group-users').append(removeHtml);
+  });
+
+  $(document).on('click', '.chat-group-user__btn--remove', function(e){
+    $(this).parent().remove();
+  });
+
+  var beforeWord = "";
+  $('#user-search-field').on('keyup', function(e) {
+    var keyword = $(this).val();
+    if(beforeWord != keyword){
+      beforeWord = keyword;
+      $.ajax({
+        type: 'GET',
+        url: '/users/search',
+        data: {keyword: keyword},
+        dataType: 'json'
+      })
+      .done(function(data) {
+        var html = "";
+        var usersList = $('#user-search-result li');
+        if(usersList)usersList.remove();
+        if(keyword.length != 0)
+        $.each(data.users, function(i,user){
+          html += buildHTML(user);
+        });
+        $('#user-search-result').append(html);
+      })
+      .fail(function() {
+        alert("error");
+      });
+    }
+    return false;
+  });
+});
